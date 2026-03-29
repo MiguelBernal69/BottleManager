9c32755f9c565c10b630b5912659ea269d436304
MiguelBernal69 <mbbernal1999@gmail.com>
2026-03-28 22:38:22 -0400

    delete ventas

--- backend/src/routes/ventas.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import * as ctrl from '../controllers/ventasController';
-const router = Router();
-router.get('/', ctrl.getAll);
-router.get('/:id', ctrl.getById);
-router.post('/', ctrl.create);
-router.patch('/:id/estado', ctrl.updateEstado);
-router.delete('/:id', ctrl.remove);
-export default router;
